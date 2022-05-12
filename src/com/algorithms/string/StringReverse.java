c90785f87afbef6d17c5218d67f5d97125289f7f
Ramachandraiah Putta <ramachandraiah.putta@gmail.com>
2022-05-12 19:53:13 +0530

    reverse a string excluding special chars in string

--- src/com/algorithms/string/StringReverse.java
+++ src/com/algorithms/string/StringReverse.java
@@ -5,34 +5,29 @@ public class StringReverse {
     private String reverse(String inputString) {
 
         char[] chars = inputString.toCharArray();
-        int head = 0, length = inputString.length(), tail = length - 1;
+        int head = 0, tail = inputString.length() - 1;
         while (head < tail) {
-            while (head < length - 1 && isSpecialChar(chars[head])) {
+            if (!Character.isAlphabetic(chars[head])) {
                 head++;
-            }
-            while (tail >= 1 && isSpecialChar(chars[tail])) {
+            } else if (!Character.isAlphabetic(chars[tail])) {
+                tail--;
+            } else {
+                char temp = chars[head];
+                chars[head] = chars[tail];
+                chars[tail] = temp;
+
+                head++;
                 tail--;
             }
-            char temp = chars[head];
-            chars[head] = chars[tail];
-            chars[tail] = temp;
-
-            head++;
-            tail--;
         }
         return new String(chars);
     }
 
-    private boolean isSpecialChar(char ch) {
-        if ((ch >= 65 && ch <= 90) || (ch >= 97 && ch <= 122))
-            return false;
-        return true;
-    }
 
     public static void main(String[] args) {
         StringReverse stringReverse = new StringReverse();
         String inputString = "abc/defgh$ij";
-        inputString = "######ab";
+        inputString = "########";
         System.out.println(stringReverse.reverse(inputString));
     }
 
